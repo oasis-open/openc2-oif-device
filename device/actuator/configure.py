fd18f29c12708d9a30c41502d0c3fd83e24281e3
Jerome Czachor <jerome.czachor@g2-inc.com>
2019-07-08 17:46:02 +0000

    Update from repo Commit - 2019-07-08 17:46:02 UTC [CI Skip]

--- device/actuator/configure.py
+++ device/actuator/configure.py
@@ -184,7 +184,7 @@ CONFIG = FrozenDict(
 
 Args = {key: value.strip() for key, value in os.environ.items()}
 Args.update(safe_load(CONFIG.ArgFiles.Default))
-Args['BASE_NAME'] = Args.get('BASE_IMAGE_NAME', Args.get('IMAGE_NAME', 'oif/base'))
+Args['BASE_NAME'] = Args.get('BASE_IMAGE_NAME', Args.get('IMAGE_NAME', 'g2inc/oif-python:latest'))
 
 CONFIG = FrozenDict(
     **CONFIG,
