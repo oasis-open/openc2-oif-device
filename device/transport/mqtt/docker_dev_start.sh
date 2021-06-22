1666b2d202e3adb8baab618ead7fa1de556c1c58
Jerome Czachor <jerome.czachor@hii-tsd.com>
2021-06-22 10:10:29 -0400

    merge master repos

--- device/transport/mqtt/docker_dev_start.sh
+++ device/transport/mqtt/docker_dev_start.sh
@@ -2,4 +2,4 @@
 dockerize -wait tcp://$QUEUE_HOST:$QUEUE_PORT -wait tcp://$ETCD_HOST:$ETCD_PORT -timeout 30s
 
 echo "Running MQTT Transport Module."
-python3 -u mqtt_transport.py
+python3 -u ./MQTT/mqtt_transport.py
